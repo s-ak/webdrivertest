6dc8d6ce8d377ea0f637db1dfb9a037d40843f39
s-ak <netsgr8_4us@yahoo.com>
2014-08-03 23:02:47 +0200

    2

--- github/elements/Base0.js
+++ github/elements/Base0.js
@@ -1,48 +1,77 @@
 var Page = require('../stack/Page'),
-    assert      = require('assert');
+    assert = require('assert');
 fs = require("fs");
 
-function Base0(){
+function Base0() {
     console.log("base 0 element made")
 };
 Base0.constructor = Base0;
 Base0.prototype = new Page();
 
-Base0.prototype.init = function(url){
+Base0.prototype.init = function(url) {
     this.url = url;
     var jsonfile = 'Base0.json';
-    content = {'elements':{'.header-logo-wordmark':{'asserts':{'height' : 32,'width':89}}},'css':{{'a[href="/plans"]':{'asserts':{'height' : 32,'width':89}}}};
+    //TODO read from json
+    this.content = {
+        'elements': {
+            'header' {
+                '.header-logo-wordmark': {
+                    'asserts': {
+                        'height': {
+                            'value': 32,
+                            'op': '==='
+                        },
+                        'width': {
+                            'value': 89,
+                            'op': '==='
+                        }
+                    }
+                }
+            }
+        }
+        'css': {
+            'plans': {
+                'selector': 'a[href="/plans"]',
+                'propName': 'color',
+                'asserts': {
+                    'rgba(65,131,196,1)': {
+                        'op': '==='
+                    }
+                }
+            }
+        }
+    };
     /*var file = (JSON.parse(fs.readFileSync(jsonfile, 'utf8')));*/
     /*this.homeLink = ['.header-logo-wordmark','class'];*/
     this.setBaseURL(url)
-        return this
+    return this
 };
 module.exports = Base0
 
-describe('home link', function(){
+describe('home link', function() {
 
-        this.timeout(99999999);
-        var handle = {};
-        /*this.element = new Base0()*/
+    this.timeout(99999999);
+    var handle = {};
+    /*this.element = new Base0()*/
 
-        before(function(done){
-            console.log("base 0 element made 0")
-            this.base0 = new Base0().init('https://github.com/')
-            this.section = this.base0.navigate()
-            console.log("base 0 element made")
-            done()
-            });
+    before(function(done) {
+        console.log("base 0 element made 0")
+        this.base0 = new Base0().init('https://github.com/')
+        this.section = this.base0.navigate()
+        console.log("base 0 element made")
+        done()
+    });
 
-        it('Github test',function(done) {
-            /*handle*/
-            /*.url('https://github.com/')*/
+    it('Github test', function(done) {
+        /*handle*/
+        /*.url('https://github.com/')*/
 
-            this.base0.page
+        this.base0.page
             .getElementSize('.header-logo-wordmark', function(err, result) {
                 assert(err === null);
                 assert(result.height === 32);
-                assert(result.width  === 89);
-                })
+                assert(result.width === 89);
+            })
             .getTitle(function(err, title) {
                 console.log(title)
                 assert(err === null);
@@ -50,15 +79,15 @@ describe('home link', function(){
                 console.log(title)
                 assert(title === 'GitHub · Build software better, together.');
                 handle.title = title
-                })
-            .getCssProperty('a[href="/plans"]', 'color', function(err, result){
+            })
+            .getCssProperty('a[href="/plans"]', 'color', function(err, result) {
                 assert(err === null);
                 /*assert(result === 'rgba(65,131,196,1)');*/
-                })
+            })
             .call(done);
-        });
+    });
 
-        after(function(done) {
-                this.base0.browser.handle.end(done);
-                });
+    after(function(done) {
+        this.base0.browser.handle.end(done);
+    });
 });
